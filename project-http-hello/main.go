48a6da15d858c4161a8c24c0fc062628159fad76
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2024-12-20 14:51:43 -0500

    exercise 05/06 projoct-http-bad-request

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -8,6 +8,10 @@ import (
 
 func yourFunction(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	fmt.Fprint(w, "Hello, ", name)
 }
 
