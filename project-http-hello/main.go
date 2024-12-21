3d7f0977e7f7d4dffed58a2223e32320b2fbd785
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2024-12-21 14:37:04 -0500

    exercise 07/05 project-http-stats

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -6,13 +6,30 @@ import (
 	"net/http"
 )
 
+var (
+	calls = []string{}
+	stats = map[string]int{}
+)
+
+func updateServerStats(name string) {
+	calls = append(calls, name)
+	_, ok := stats[name]
+	if !ok {
+		stats[name] = 0
+	}
+	stats[name] += 1
+}
+
 func yourFunction(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+	updateServerStats(name)
 	if name == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	fmt.Fprint(w, "Hello, ", name)
+	fmt.Printf("calls: %#v\n", calls)
+	fmt.Printf("stats: %#v\n\n", stats)
 }
 
 func main() {
