fe4cc0acd64fcd5167dbfe5ab20677fab6d5e342
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2024-12-24 02:31:53 -0500

    exercise 12/05 project-ls-hidden

--- project-ls/main.go
+++ project-ls/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	testdata := "testdata"
 	files := listFiles(testdata)
 	for _, f := range files {
+		if strings.HasPrefix(f, ".") {
+			continue
+		}
 		fmt.Println(f)
 	}
 }
